2c6a9a3caa68fa5ab1e9dc1c3e97c5a613287960
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 17:13:24 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+// Q116
+// create a switch case that matches several casses  to the same code block 
+// this fuction log the season based on the month
+function season(month: string) {
+  switch (month) {
+    case "December":
+    case "January":
+    case "February":
+      console.log("Winter");
+      break;
+    case "March":
+    case "April":
+    case "May":
+      console.log("Spring");
+      break;
+    case "June":
+    case "July":
+    case "August":
+      console.log("Summer");
+      break;
+    case "September":
+    case "October":
+    case "November":
+      console.log("Autumn");
+      break;
+  }
+}
+season("December")
